ba237703db4a862f910fb732decb6fc8ff9c9e9c
LLLgoyour <lllgoyour@protonmail.com>
2024-11-02 08:14:05 -0600

    Update comments

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 """
 Team 63344Z
+Robot Yaju
 Season 2024-2025.
 """
 
@@ -11,14 +12,14 @@ brain = Brain()
 
 
 # region config
-controller = Controller(PRIMARY)  # Primary controller
-motor_left_front = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)  # Left front motor
-motor_right_front = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)  # Right front motor
-motor_left_back = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True)  # Left back motor
-motor_right_back = Motor(Ports.PORT2, GearSetting.RATIO_18_1, True)  # Right back motor
-intake = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)  # intake motor
-sender = Motor(Ports.PORT7, GearSetting.RATIO_18_1, True)  # sender motor
-p = DigitalOut(brain.three_wire_port.a)  # Pneumatic motor
+controller = Controller(PRIMARY)  # primary controller
+motor_left_front = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)  # left front motor
+motor_right_front = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)  # right front motor
+motor_left_back = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True)  # left back motor
+motor_right_back = Motor(Ports.PORT2, GearSetting.RATIO_18_1, True)  # right back motor
+intake = Motor(Ports.PORT5, GearSetting.RATIO_18_1, True)  # intake motor
+sender = Motor(Ports.PORT11, GearSetting.RATIO_18_1, True)  # sender motor
+p = DigitalOut(brain.three_wire_port.a)  # pneumatic motor
 
 
 # gyro
@@ -31,114 +32,104 @@ mgr = MotorGroup(motor_right_front, motor_right_back)
 # smartDrive = SmartDrive(mgl, mgr, Gyro, 319.19, 3600, 3000, MM, 1.4) # no gyro sensor
 # end of region config
 
-# start of initialization
-global FULL_SPEED
+# Constants
 FULL_SPEED = 100
-# end of initialization
+
+# Global variables
+valve_open = False
 
 def initialize_autonomous():
-    # start the autonomous control tasks
+    """
+    Initialize the autonomous control tasks.
+    """
     _autonomous_thread = Thread(autonomous)
-    # wait for the driver control period to end
+
     while (competition.is_autonomous() and competition.is_enabled()):
-        # wait 10 milliseconds before checking again
         wait(10, MSEC)
-    # stop the autonomous control tasks
+        
     _autonomous_thread.stop()
 
 def initialize_driver_control():
-    # start the driver control tasks
+    """
+    Initialize the driver control tasks.
+    """
     _driver_thread = Thread(driver_control)
 
-    # wait for the driver control period to end
     while (competition.is_driver_control() and competition.is_enabled()):
-        # wait 10 milliseconds before checking again
         wait(10, MSEC)
 
     # stop the driver control tasks
     _driver_thread.stop()
 
 def autonomous():
-    # TBD
+    """
+    Autonomous control function.
+    """
+    # TODO: implement autonomous control
     pass
 
 def driver_control():
     """
-    intake_control: done
-    drivetrain_control: done
-    pneumatic_control: done
-    sender_control: TBD
-    climb_control: TBD
+    Driver control function.
     """
     while True:
         intake_control()
         drivetrain_control()
         pneumatic_control()
         sender_control()
-        climb_control()
+        climb_control() # TODO
         
-        # wait for the driver control period to end
-        wait(20, MSEC)
-
-        if controller.buttonLeft.pressing():
-            pass
-            
-        # Pneumatic control
-        valve_open = False
-        # L2 button: 
-        if controller.buttonL2.pressing(): 
-            ###if not valve_open:
-                # p1.open()
-                # valve_open = True
-                # wait(1000, MSEC)
-            # else:
-                # p1.close()
-                # valve_open = False
-                # wait(1000, MSEC)
-            pass
+        wait(10, MSEC)
         
-        if controller.buttonL1.pressing():
-            pass
-                
-        if controller.buttonUp.pressing():
-            pass
-
-        if controller.buttonDown.pressing():
-            pass
-        else:
-            pass
-    
 def intake_control():
-    """ intake control
-    R1 button: switch intake direction
-    R2 button: switch intake on/off
+    """
+    Control the intake motor based on controller input.
     """
     if controller.buttonR1.pressing():  # R1 button: intake backward
         intake.spin(FORWARD, FULL_SPEED, PERCENT)
     elif controller.buttonR2.pressing():  # R2 button: intake forward
         intake.spin(REVERSE, FULL_SPEED, PERCENT)
     else:
-        intake.stop(COAST)
+        intake.spin(REVERSE, 40, PERCENT)
     
 def drivetrain_control():
-        # Controller Axis 1 and Axis 3:
-        # The left front motor and left back motor spin FORWARD when pushing the left joystick FORWARD.
-        # Then, the left front motor and left back motor spin backward when pushing the left joystick backward.
-        motor_left_front.spin(FORWARD, controller.axis1.value() + controller.axis3.value(), PERCENT)
-        motor_left_back.spin(FORWARD, controller.axis1.value() + controller.axis3.value(), PERCENT)
-        motor_right_front.spin(FORWARD, controller.axis1.value() - controller.axis3.value(), PERCENT)
-        motor_right_back.spin(FORWARD, controller.axis1.value() - controller.axis3.value(), PERCENT)
+    """
+    Control the drivetrain motors based on controller input.
+    """
+    motor_left_front.spin(FORWARD, controller.axis1.value() + controller.axis3.value(), PERCENT)
+    motor_left_back.spin(FORWARD, controller.axis1.value() + controller.axis3.value(), PERCENT)
+    motor_right_front.spin(FORWARD, controller.axis1.value() - controller.axis3.value(), PERCENT)
+    motor_right_back.spin(FORWARD, controller.axis1.value() - controller.axis3.value(), PERCENT)
 
 def pneumatic_control():
-    p.set(False)
+    """
+    Control the pneumatic valve based on controller input.
+    """
+    global valve_open
+    
     if controller.buttonL1.pressing():
-        p.set(True)
+        valve_open = True
     elif controller.buttonL2.pressing():
-        p.set(False)
+        valve_open = False
+        
+    p.set(valve_open)
 
 def sender_control():
-    pass
+    """
+    Control the sender motor based on controller input.
+    """
+    if controller.buttonUp.pressing():
+        sender.spin(FORWARD, FULL_SPEED, PERCENT)
+    elif controller.buttonDown.pressing():
+        sender.spin(REVERSE, FULL_SPEED, PERCENT)
+    else:
+        sender.stop(COAST)
+        
 def climb_control():
+    """
+    Control the climbing mechanism.
+    """
+    #TODO: implement climbing mechanism control
     pass
 
 # allows access to Competition methods
